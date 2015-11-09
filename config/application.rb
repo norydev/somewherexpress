53ad0283c5eaecdb13ba06e36fd3b9259bd9c7ee
Fabien Loup <info@norydev.com>
2015-11-09 16:47:21 +0200

    app_generators

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,20 @@ Bundler.require(*Rails.groups)
 
 module Somewherexpress
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = true
+    config.embed_authenticity_token_in_remote_forms = true
+
+    config.generators do |generate|
+      generate.helper true
+      generate.javascript_engine false
+      generate.request_specs false
+      generate.routing_specs false
+      generate.stylesheets false
+      generate.test_framework :rspec
+      generate.view_specs false
+    end
+
+    config.action_controller.action_on_unpermitted_parameters = :raise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
