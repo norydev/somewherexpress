de55975442fa8f7572d9992f179b799c648d25bc
Fabien Loup <info@norydev.com>
2015-11-15 11:59:22 +0200

    registrations date on card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def date_format(date)
     l(date, format: "%d %B %Y") if date
   end
+
+  def datetime_format(datetime)
+    l(datetime, format: "%d %B %Y à %H:%M") if datetime
+  end
 end
--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -37,4 +37,12 @@ module CompetitionsHelper
       "<div class='ribbon-blue'>#{t('closed')}</div>".html_safe
     end
   end
+
+  def registrations(competition)
+    if competition.registrations_open?
+      "<div class='panel-footer text-uppercase small text-center'>#{t('competitions.registrations.end', time: distance_of_time_in_words(competition.end_registration || (competition.start_date - 1), Time.now))}:<br> #{(datetime_format(competition.end_registration) || datetime_format(competition.start_date - 1))}</div>".html_safe
+    elsif !competition.registrations_open? && !competition.finished
+      "<div class='panel-footer text-uppercase small text-center'>#{t('competitions.registrations.start', time: distance_of_time_in_words(competition.start_registration, Time.now))}:<br>#{datetime_format(competition.start_registration)}</div>".html_safe
+    end
+  end
 end
