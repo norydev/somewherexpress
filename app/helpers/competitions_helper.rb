53a9d3843405a96a3150d18ca7cea83b72bd83da
Fabien Loup <info@norydev.com>
2015-11-21 16:49:20 +0200

    icomoon

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -11,7 +11,7 @@ module CompetitionsHelper
 
   def track_result(rank)
     if rank.try(:result) == 1
-      image_tag 'medal.svg', class: 'medal'
+      '<span class="medals"><i class="icon-medal"></i></span>'.html_safe
     else
       rank.try(:result)
     end
