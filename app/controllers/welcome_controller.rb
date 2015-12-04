cc7ed6dda122758f2fd3fe905fec2a593d6c2a98
Fabien Loup <info@norydev.com>
2015-12-05 00:18:37 +0200

    space in curly brackets

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,14 +4,14 @@ class WelcomeController < ApplicationController
   skip_after_action :verify_policy_scoped
 
   def index
-    @markers = Gmaps4rails.build_markers(City.on_map.uniq{ |m| "#{m[:lat]}-#{m[:lng]}" }) do |city, marker|
+    @markers = Gmaps4rails.build_markers(City.on_map.uniq { |m| "#{m[:lat]}-#{m[:lng]}" }) do |city, marker|
       marker.lat city.lat
       marker.lng city.lng
       marker.picture({
           "url" => ActionController::Base.helpers.asset_path("marker.svg"),
           "width" =>  32,
-          "height" => 32})
-      marker.infowindow render_to_string(partial: "/welcome/map_box", locals: {cities: City.where(lat: city.lat, lng: city.lng), end_cities: City.where(lat: city.lat, lng: city.lng, localizable_type: "Track", order: "end")})
+          "height" => 32 })
+      marker.infowindow render_to_string(partial: "/welcome/map_box", locals: { cities: City.where(lat: city.lat, lng: city.lng), end_cities: City.where(lat: city.lat, lng: city.lng, localizable_type: "Track", order: "end") })
     end
     if user_signed_in?
       render 'dashboard'
