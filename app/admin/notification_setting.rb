ea5a77a491d0c69a8842b349cd14464fc0bbdb7f
Fabien Loup <info@norydev.com>
2016-04-03 13:11:17 +0300

    notifications in admin panel

---
+++ app/admin/notification_setting.rb
@@ -0,0 +1,36 @@
+ActiveAdmin.register NotificationSetting do
+
+  permit_params :user_id, :locale, :as_user_new_competition,
+      :as_user_competition_edited, :as_user_new_subscription,
+      :as_user_subscription_satus_changed, :as_author_new_subscription,
+      :as_author_cancelation
+
+  index do
+    selectable_column
+    column :user
+    column :locale
+    column :as_user_new_competition
+    column :as_user_competition_edited
+    column :as_user_new_subscription
+    column :as_user_subscription_satus_changed
+    column :as_author_new_subscription
+    column :as_author_cancelation
+    actions
+  end
+
+  form do |f|
+    f.inputs "Notifications" do
+      f.input :user
+      f.input :locale, collection: [['English', 'en'],['French', 'fr']]
+      f.input :as_user_new_competition
+      f.input :as_user_competition_edited
+      f.input :as_user_new_subscription
+      f.input :as_user_subscription_satus_changed
+      f.input :as_author_new_subscription
+      f.input :as_author_cancelation
+    end
+
+    f.actions
+  end
+
+end
