7e1b2fbd54f2f290373968f754a189e35c68beca
Fabien Loup <info@norydev.com>
2016-04-09 17:03:39 +0300

    new: subscription to author and updates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,23 +11,13 @@ class UserMailer < ApplicationMailer
     mail(from: "SomewherExpress <info@somewherexpress.com>", to: @user.email, subject: t('.subject'))
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.goodbye.subject
-  #
   def goodbye(user_id)
     @user = User.find(user_id)
 
     mail(from: "SomewherExpress <info@somewherexpress.com>", to: @user.old_email, subject: t('.subject'))
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.new_competition.subject
-  #
-  def new_competition(user_id, competition_id)
+  def as_user_new_competition(user_id, competition_id)
     @user = User.find(user_id)
     @competition = Competition.find(competition_id)
 
@@ -36,12 +26,7 @@ class UserMailer < ApplicationMailer
     end
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.competition_edited.subject
-  #
-  def competition_edited(user_id, competition_id)
+  def as_user_competition_edited(user_id, competition_id)
     @user = User.find(user_id)
     @competition = Competition.find(competition_id)
 
@@ -51,11 +36,6 @@ class UserMailer < ApplicationMailer
 
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.competition_edited.subject
-  #
   def new_subscription(user_id, competition_id)
     @user = User.find(user_id)
     @competition = Competition.find(competition_id)
@@ -65,13 +45,9 @@ class UserMailer < ApplicationMailer
     end
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.competition_edited.subject
-  #
-  def new_to_my(user_id, competition_id)
-    @user = User.find(user_id)
+  def as_author_new_subscription(participant_id, competition_id, author_id)
+    @user = User.find(author_id)
+    @participant = User.find(participant_id)
     @competition = Competition.find(competition_id)
 
     I18n.with_locale(@user.notification_setting.locale) do
@@ -79,11 +55,6 @@ class UserMailer < ApplicationMailer
     end
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.competition_edited.subject
-  #
   def status_changed(user_id, competition_id)
     @user = User.find(user_id)
     @competition = Competition.find(competition_id)
@@ -93,11 +64,6 @@ class UserMailer < ApplicationMailer
     end
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.competition_edited.subject
-  #
   def cancelation(user_id, competition_id)
     @user = User.find(user_id)
     @competition = Competition.find(competition_id)
