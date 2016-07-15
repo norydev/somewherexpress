354900b72f81efe3517f29c3c6c97692f634374a
Fabien Loup <info@norydev.com>
2016-07-15 14:06:56 +0300

    fb login notifications & welcome

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,8 +5,10 @@ module Users
       user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
       if user.persisted?
+        NotificationSetting.create!(user: user, locale: params[:locale] || :fr)
+        UserMailer.welcome(user.id).deliver_later
+
         sign_in_and_redirect user, event: :authentication
-        # set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
       else
         session["devise.facebook_data"] = request.env["omniauth.auth"]
         redirect_to new_user_registration_url
