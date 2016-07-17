9521b283e2b9a282b80a6f23cbb0d55e29e058b3
Fabien Loup <info@norydev.com>
2016-07-17 13:33:56 +0300

    new api key

--- lib/tasks/cities.rb
+++ lib/tasks/cities.rb
@@ -1,7 +1,7 @@
 namespace :cities do
   desc "fetch picture for existing cities"
   task fetch_picture: :environment do
-    client = GooglePlaces::Client.new(ENV["GMAPS"])
+    client = GooglePlaces::Client.new(ENV["GOOGLE-SERVER"])
     City.all.each do |city|
       spot = client.spots(city.lat, city.lng).first
       picture = spot.photos.first.fetch_url(800)
