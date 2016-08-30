c29fc4f2bda71293ec7d82f22bc2b45c7ca99a05
Fabien Loup <info@norydev.com>
2016-08-30 22:12:28 +0300

    rubbish test

---
+++ spec/concepts/track/update_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe Track::Update do
+  let!(:user) { FactoryGirl.create(:user) }
+
+  it "updates a competition" do
+    competition = Competition::Create
+                  .call(competition: {
+                          name: "new competition",
+                          published: "1",
+                          start_date: 2.weeks.from_now.to_s,
+                          end_date: 3.weeks.from_now.to_s,
+                          start_registration: Time.current,
+                          finished: false,
+                          start_city: { name: "Yverdon, CH",
+                                        locality: "Yverdon-Les-Bains",
+                                        country_short: "CH" },
+                          end_city: { name: "Berne, CH",
+                                      locality: "Berne",
+                                      country_short: "CH" },
+                          tracks: [{ start_time: 16.days.from_now.to_s,
+                                     start_city: { name: "Yverdon, CH",
+                                                   locality: "Yverdon-Les-Bains",
+                                                   country_short: "CH" },
+                                     end_city: { name: "Berne, CH",
+                                                 locality: "Berne",
+                                                 country_short: "CH" } }]
+                        },
+                        current_user: user)
+                  .model
+
+    4.times do
+      u = FactoryGirl.create(:user)
+      FactoryGirl.create(:subscription, user: u, competition: competition,
+                                        rules: "1")
+    end
+
+    track = competition.tracks.first
+
+    Track::Update.call(id: track.id,
+                       track: { ranks: [{ id: track.ranks[0].id,
+                                          points: 2,
+                                          result: 1,
+                                          dsq: "0" },
+                                        { id: track.ranks[1].id,
+                                          points: 2,
+                                          result: 1,
+                                          dsq: "0" },
+                                        { id: track.ranks[2].id,
+                                          points: 1,
+                                          result: 2,
+                                          dsq: "0" },
+                                        { id: track.ranks[3].id,
+                                          points: 0,
+                                          result: 0,
+                                          dsq: "1" }] })
+
+    track.reload
+    expect(track.ranks.where(result: 1).size).to eq 2
+    expect(track.ranks.where(points: 1).size).to eq 1
+    expect(track.ranks.where(dsq: true).size).to eq 1
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -35,10 +35,10 @@
 
 FactoryGirl.define do
   factory :user do
-    email       Faker::Internet.email
+    email       { Faker::Internet.email }
     password    "12345678"
-    first_name  Faker::Name.first_name
-    last_name   Faker::Name.last_name
+    first_name  { Faker::Name.first_name }
+    last_name   { Faker::Name.last_name }
     picture     "https://unsplash.it/200/200"
     girl        false
     organizer   true
