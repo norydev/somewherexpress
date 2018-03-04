a05fa3ddc95d8994fd94c0bd81581a498478efee
Fabien Loup <info@norydev.com>
2018-03-04 20:27:49 +0300

    remove finished competitions map from dashboard + some n+1 refactoring, nicer background of maps on competitions page, useless comment

--- app/assets/javascripts/components/map.js
+++ app/assets/javascripts/components/map.js
@@ -1,18 +1,3 @@
-function gmapize(markers) {
-  handler = Gmaps.build('Google', { markers: { clusterer: undefined  }});
-  handler.buildMap({
-  provider: {
-    disableDefaultUI: true,
-    zoomControl: true,
-    scrollwheel: false
-  },
-    internal: { id: 'map' } }, function(){
-    SeMarkers = handler.addMarkers(markers);
-    handler.bounds.extendWith(SeMarkers);
-    handler.fitMapToBounds();
-  });
-}
-
 function mapWithDirectionsAndMarkers(routes, markers) {
   var directionsDisplays = [];
   var directionsService = new google.maps.DirectionsService();
