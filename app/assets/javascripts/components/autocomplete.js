94039fce29ed35d42ce1694ed71afcefe164dbff
Fabien Loup <info@norydev.com>
2018-03-04 23:00:23 +0300

    min js cleaning

--- app/assets/javascripts/components/autocomplete.js
+++ app/assets/javascripts/components/autocomplete.js
@@ -2,27 +2,26 @@ function initializeAutocomplete(id) {
   var element = document.getElementById(id);
   if (element) {
     var autocomplete = new google.maps.places.Autocomplete(element, { types: ['(cities)'] });
-    google.maps.event.addListener(autocomplete, 'place_changed', function(){
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
       var place = autocomplete.getPlace();
 
       var pic_field = document.getElementById(id.replace( /name/g, '' ) + "picture");
-      pic_field.value = place.photos[0].getUrl({'maxWidth': 800});
+      pic_field.value = place.photos[0].getUrl({ 'maxWidth': 800 });
 
-      for (var i in place.address_components) {
-        var component = place.address_components[i];
-        for (var j in component.types) {
+      _.each(place.address_components, function(component) {
+        _.each(component.types, function(type) {
           // Long name for country and admn area
-          var type_element = document.getElementById(id.replace( /name/g, '' ) + component.types[j]);
+          var type_element = document.getElementById(id.replace( /name/g, '' ) + type);
           if (type_element) {
             type_element.value = component.long_name;
           }
           // Short name for country and admn area
-          var type_element = document.getElementById(id.replace( /name/g, '' ) + component.types[j] + '_short');
+          var type_element = document.getElementById(id.replace( /name/g, '' ) + type + '_short');
           if (type_element) {
             type_element.value = component.short_name;
           }
-        }
-      }
+        });
+      });
     });
   }
 }
