f3ea1b1d3788ac2f474633f0836619ae6e9e4e88
Fabien Loup <info@norydev.com>
2018-03-05 18:06:33 +0300

    DRY the map javascript

--- app/assets/javascripts/components/map.js
+++ app/assets/javascripts/components/map.js
@@ -1,67 +1,4 @@
-function mapWithDirectionsAndMarkers(routes, markers) {
-  var directionsDisplays = [];
-  var directionsService = new google.maps.DirectionsService();
-
-  _.each(routes, function(route) {
-    var directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true,
-                                                                 preserveViewport: true });
-
-    var waypts = [];
-    _.each(route.tracks, function(track) {
-      if (track.end_city.name != route.end_city.name) {
-        waypts.push({
-          location: track.end_city.name,
-          stopover: true
-        });
-      }
-    });
-
-    function calcRoute() {
-      var origin      = new google.maps.LatLng(route.start_city.lat, route.start_city.lng);
-      var destination = new google.maps.LatLng(route.end_city.lat, route.end_city.lng);
-      var request = {
-          origin:      origin,
-          destination: destination,
-          waypoints:   waypts,
-          travelMode:  google.maps.TravelMode.DRIVING
-      };
-      directionsService.route(request, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-          directionsDisplay.setDirections(response);
-        }
-      });
-    }
-    calcRoute();
-
-    directionsDisplays.push(directionsDisplay)
-  });
-
-
-  handler = Gmaps.build('Google', { markers: { clusterer: undefined }});
-  handler.buildMap({
-  provider: {
-    disableDefaultUI: true,
-    zoomControl: true,
-    scrollwheel: false,
-    styles: [{ "featureType": "road",
-               "elementType": "labels.icon",
-               "stylers": [ { "visibility": "off" } ]
-             }]
-  },
-  internal: { id: 'map' } }, function(){
-    _.each(directionsDisplays, function(directionsDisplay) {
-      directionsDisplay.setMap(handler.getMap());
-    });
-    SeMarkers = handler.addMarkers(markers);
-    handler.bounds.extendWith(SeMarkers);
-    handler.fitMapToBounds();
-  });
-}
-
-function mapWithDirections(route, markers) {
-  var directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true });
-  var directionsService = new google.maps.DirectionsService();
-
+function setDirection(route, directionsService, directionsDisplay) {
   var waypts = [];
   _.each(route.tracks, function(track) {
     if (track.end_city.name != route.end_city.name) {
@@ -75,7 +12,6 @@ function mapWithDirections(route, markers) {
   function calcRoute() {
     var origin      = new google.maps.LatLng(route.start_city.lat, route.start_city.lng);
     var destination = new google.maps.LatLng(route.end_city.lat, route.end_city.lng);
-
     var request = {
       origin:      origin,
       destination: destination,
@@ -89,8 +25,11 @@ function mapWithDirections(route, markers) {
     });
   }
   calcRoute();
+  return directionsDisplay
+}
 
-  handler = Gmaps.build('Google', { markers: { clusterer: undefined  }});
+function buildMap(callback) {
+  handler = Gmaps.build('Google', { markers: { clusterer: undefined }});
   handler.buildMap({
   provider: {
     disableDefaultUI: true,
@@ -98,7 +37,39 @@ function mapWithDirections(route, markers) {
     scrollwheel: false,
     styles: [{ featureType: "road", elementType: "labels.icon", stylers: [{ visibility: "off" }] }]
   },
-  internal: { id: 'map' } }, function() {
+  internal: { id: 'map' } }, callback);
+}
+
+function mapWithDirectionsAndMarkers(routes, markers) {
+  var directionsDisplays = [];
+  var directionsService = new google.maps.DirectionsService();
+
+  _.each(routes, function(route) {
+    var directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true,
+                                                                 preserveViewport: true });
+
+    directionsDisplay = setDirection(route, directionsService, directionsDisplay)
+
+    directionsDisplays.push(directionsDisplay)
+  });
+
+  buildMap(function(){
+    _.each(directionsDisplays, function(directionsDisplay) {
+      directionsDisplay.setMap(handler.getMap());
+    });
+    SeMarkers = handler.addMarkers(markers);
+    handler.bounds.extendWith(SeMarkers);
+    handler.fitMapToBounds();
+  });
+}
+
+function mapWithDirections(route, markers) {
+  var directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true });
+  var directionsService = new google.maps.DirectionsService();
+
+  directionsDisplay = setDirection(route, directionsService, directionsDisplay)
+
+  buildMap(function() {
     markers = handler.addMarkers(markers);
     directionsDisplay.setMap(handler.getMap());
   });
